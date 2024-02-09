b0d391be8ed4f743f138a62d3fbe3e9c2078a308
Olga Cazacioc <1697327@cstjean.qc.ca>
2024-02-09 17:48:58 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routing/>
+      <Footer />
     </>
   );
 }
